42b22831a384f928c74cde97bb067ea9cbb12fd5
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-03-02 15:08:15 +0200

    feat: add input box

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -7,16 +7,16 @@ interface Props {
 const InputBox = ({ value, onChange, onSend }: Props) => {
     return (
         <div className="p-4 bg-blitzWhite border-t border-blitzBlue flex">
-        <input
-        type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        className="flex-1 p-2 border border-blitzBlue rounded-1 text-blitzBlack focus:outline-none"
-        placeholder="Type here..."
-        />
-        <button onClick={onSend} className="bg-blitzBlue text-blitzWhite p-2 rounded-r">
-        Send
-        </button>
+            <input
+                type="text"
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                className="flex-1 p-2 border border-blitzBlue rounded-1 text-blitzBlack focus:outline-none"
+                placeholder="Type here..."
+            />
+            <button onClick={onSend} className="bg-blitzBlue text-blitzWhite p-2 rounded-r">
+                Send
+            </button>
         </div>
     );
 };
