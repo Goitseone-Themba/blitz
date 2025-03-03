d764b91813708b6f273b167c40c96a65c61e3bc7
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-03-04 01:21:40 +0200

    feat: increase text area

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -7,12 +7,12 @@ interface Props {
 const InputBox = ({ value, onChange, onSend }: Props) => {
     return (
         <div className="p-4 bg-blitzWhite border-t border-blitzBlue flex">
-            <input
-                type="text"
+            <textarea
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
-                className="flex-1 p-2 border border-blitzBlue rounded-1 text-blitzBlack focus:outline-none"
+                className="flex-1 p-2 border border-blitzBlue rounded-1 text-blitzBlack focus:outline-none resize-none"
                 placeholder="Type here..."
+                rows={2}
             />
             <button onClick={onSend} className="bg-blitzBlue text-blitzWhite p-2 rounded-r">
                 Send
