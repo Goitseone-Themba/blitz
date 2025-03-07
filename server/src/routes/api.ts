a0b0c5f255250b67bf34fb60d02bf5ad38841842
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-03-07 18:28:04 +0200

    feat(server): add routes

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { handleChat, getChat, createBookmark, getBookmarks, deleteBookmark, } from "../controllers/chat";
+import { createNote, getNotes } from '../controllers/notes';
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.get('/chat/:chatId', getChat);
 router.post('/bookmark', createBookmark);
 router.get('/bookmarks', getBookmarks);
 router.delete('/bookmark/:bookmarkId', deleteBookmark);
+router.post('/notes', createNote);
+router.get('/notes', getNotes);
 
 export const chatRoutes = router;
